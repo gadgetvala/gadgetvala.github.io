8a80a1dcd52905afbc183904ff81701ae1a0de78
gadgetvala <surajj.verma7@gmail.com>
2019-11-14 11:35:32 +0530

    Added Type Writer Effect

--- js/main.js
+++ js/main.js
@@ -14,19 +14,18 @@
             cross2.style.opacity = '1';
             cross1.style.transform = 'rotate(0deg) translateX(-50%) translateY(0px)';
             cross1.style.transformOrigin = 'left';
-            
+
             cross3.style.transform = 'rotate(0deg) translateX(-50%) translateY(0px)';
             cross3.style.transformOrigin = 'left';
 
             menuBackground.style.transform = 'scale(1)';
             list.style.fontSize = '0';
             list.style.left = '0';
-
         } else {
             cross2.style.opacity = '0';
             cross1.style.transform = 'rotate(45deg) translateX(-6px) translateY(9px)';
             cross1.style.transformOrigin = 'left center 0px';
-            
+
             cross3.style.transform = 'rotate(-45deg) translateY(-10px) translateX(-7px)';
             cross3.style.transformOrigin = 'left center 0px';
 
@@ -34,7 +33,19 @@
             list.style.fontSize = '5rem';
             list.style.left = '50%';
             window.style.overflow = 'hidden';
-
         }
     });
 }
+
+/**********************
+ **Type Writer Effect**
+ **********************/
+var options = {
+    strings: ['Designer.', 'Fronted Developer.', 'Backend Developer.', 'Photographer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity
+};
+
+var typed = new Typed('.element', options);
