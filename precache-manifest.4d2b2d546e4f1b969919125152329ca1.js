4e4e7c566d34cffe0b4e0cfde343646d765cc17b
gadgetvala <surajj.verma7@gmail.com>
2020-09-22 09:38:52 +0530

    Update v2.0.3

---
+++ precache-manifest.4d2b2d546e4f1b969919125152329ca1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8f68bba23b9c99845eecbd13219e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3780ec1e0dd5c828db2",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "fee4053000740ec17378",
+    "url": "/static/css/main.3ddf8a25.chunk.css"
+  },
+  {
+    "revision": "e3780ec1e0dd5c828db2",
+    "url": "/static/js/2.27873b9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27873b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee4053000740ec17378",
+    "url": "/static/js/main.928eeaec.chunk.js"
+  },
+  {
+    "revision": "0964e8eca072fe79337f",
+    "url": "/static/js/runtime-main.339dc6ee.js"
+  },
+  {
+    "revision": "8c9a49d4287b0a6888885e1903e12f0e",
+    "url": "/static/media/Whereintheworld-min.8c9a49d4.png"
+  },
+  {
+    "revision": "a148cfa09398d6a20bd0f231a825a960",
+    "url": "/static/media/logo-white.a148cfa0.svg"
+  },
+  {
+    "revision": "b6dca49f41ac03b6e1d57253d1b8e10c",
+    "url": "/static/media/natours-min.b6dca49f.png"
+  },
+  {
+    "revision": "15db286383a80ed1a02c26bead219344",
+    "url": "/static/media/nexter-min.15db2863.png"
+  },
+  {
+    "revision": "1c533f393ae972d9ce130254a9877d48",
+    "url": "/static/media/own.1c533f39.jpeg"
+  },
+  {
+    "revision": "c1eb27404bbc0d6052620ac79194ae19",
+    "url": "/static/media/social-2.c1eb2740.svg"
+  },
+  {
+    "revision": "5dd35835bf28809a3602ded1397b1876",
+    "url": "/static/media/social-4.5dd35835.png"
+  },
+  {
+    "revision": "bf2b71cbda7077107643e68fa4d29b74",
+    "url": "/static/media/sourcerer.bf2b71cb.svg"
+  },
+  {
+    "revision": "25e63218240e54dbf249f04a725a472c",
+    "url": "/static/media/tmdbMovies-min.25e63218.png"
+  },
+  {
+    "revision": "db6739c14254ed269d7d9963ac462ef4",
+    "url": "/static/media/trillo-min.db6739c1.png"
+  },
+  {
+    "revision": "603607c9f469deab777e2eed39c4ff3a",
+    "url": "/static/media/validator-min.603607c9.png"
+  }
+]);
